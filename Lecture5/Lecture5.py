eb8316af4b013b5e00c58a0ee28ed01b066b46d3
bvelez <Bienvenido.Velez@upr.edu>
2025-09-17 13:16:42 -0400

    Added template for Lecture5

---
+++ Lecture5/Lecture5.py
@@ -0,0 +1,72 @@
+import math
+
+# UPRM CIIC 3015 Fall 2025
+# Lecture 5: Lists, Strings and Tuples
+# Live Coding Examples
+# Instructors: Karelys López and Bienvenido Vélez
+
+# Example #1 ------------------------------------------------
+# Function print_list(l) that prints all elements in list l
+
+# [YOUR FUNCTION HERE]
+
+# Tests
+# print(print_list([1,2,3,4,5,6])) # Should print [1,2,3,4,5,6]
+
+# Example #2 ------------------------------------------------
+# Function sum_of_list(l) that returns the sum of all the numbers in a list l
+
+# [YOUR FUNCTION HERE]
+
+# Tests
+# print(sum_of_list([1,2,3,4])) # Should print 10
+
+# Example #3 ------------------------------------------------
+# Function average_of_list(l) that returns the average of all the numbers in
+# list l.  Returns -1 if the list is empty.
+
+# [YOUR FUNCTION HERE]
+
+# Tests
+# print(average_of_list([1,2,3,4])) # Should print 2.5
+# print(average_of_list([])) # Should print -1
+
+
+# Example #4 ------------------------------------------------
+# Function filter_list_evens(l) that returns a new list containing all even
+# numbers in list l
+
+# [YOUR FUNCTION HERE]
+
+# Tests
+# print(filter_list_evens([1,2,3,4,5,6])) # Should print [2, 4, 6]
+
+
+# Example #5 ------------------------------------------------
+# Function filter_list_positives(l) that returns a new list containing all
+# positive numbers in list l
+
+# [YOUR FUNCTION HERE]
+
+# Tests
+# print(filter_list_positives([1,-2,3,-4,5,-6])) # Should print [1, 3, 5]
+
+
+# Example #6 ------------------------------------------------
+# Function list_to_string(l) that takes list of elements and returns new list
+# with corresponding strings
+
+# [YOUR FUNCTION HERE]
+
+
+# Example #7 ------------------------------------------------
+# Function that returns concatenation of all strings in a list
+
+# [YOUR FUNCTION HERE]
+
+
+# Tests
+
+
+
+
