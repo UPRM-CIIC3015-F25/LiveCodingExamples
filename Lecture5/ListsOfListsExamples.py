7719876be625904ae5720fbc080e44b10d189e52
bvelez <Bienvenido.Velez@upr.edu>
2025-09-29 13:47:30 -0400

    Added ListOfListsExamples file header only.

---
+++ Lecture5/ListsOfListsExamples.py
@@ -0,0 +1,8 @@
+# UPRM CIIC 3015 Fall 2025
+# Lecture 5: Lists, Strings and Tuples
+# Live Coding Examples
+# Instructors: Karelys López and Bienvenido Vélez
+
+# PART C: EXAMPLES WITH MULTI-DIMENSIONAL LISTS
+
+
