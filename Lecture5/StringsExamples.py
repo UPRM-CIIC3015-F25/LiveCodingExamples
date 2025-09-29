caba5048106f6b12164b563080dcf96ee76a2f80
bvelez <Bienvenido.Velez@upr.edu>
2025-09-29 12:52:28 -0400

    Added S string examples using loops.

---
+++ Lecture5/StringsExamples.py
@@ -0,0 +1,41 @@
+# UPRM CIIC 3015 Fall 2025
+# Lecture 5: Lists, Strings and Tuples
+# Live Coding Examples
+# Instructors: Karelys López and Bienvenido Vélez
+
+# PART B: EXAMPLES WITH STRINGS
+
+# Example 1
+# Function count_vowels(w) that returns the count of vowels in a given word
+def count_vowels(w):
+    # [ YOUR CODE HERE ]
+    pass
+
+
+
+# Example 2:
+# Function find_consonant(w) to find position of first consonant in a word w.
+# Returns -1 if w contains no consonants
+def find_consonant(w):
+    # [ YOUR CODE HERE ]
+    pass
+
+
+# Example 3:
+# Function replace_all_letters(word, letter, replacement) to return a new
+# string that replace all occurrences of a letter by a replacement in a given
+# word.
+def replace_all_letters(word, letter, substitute):
+    # [ YOUR CODE HERE ]
+    pass
+
+# Example 4:
+# Function replace_all_words(sentence, word, replacement) to return a new
+# string that replace all occurrences of a word by a replacement in a given
+# sentence.
+def replace_all_letters(word, letter, substitute):
+    # [ YOUR CODE HERE ]
+    pass
+
+
+
